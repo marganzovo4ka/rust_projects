1501867f960a8ab5c4dad58aa099031b1743de5a
marganzovo4ka <dzik04@mail.ru>
2023-07-14 20:44:26 +0300

    first try

---
+++ div_voltage/main.rs
@@ -0,0 +1,124 @@
+use std::io;
+
+fn input () -> f32 {
+
+    loop{
+    let mut x = String::new();
+
+    io::stdin().read_line(  &mut x).expect("failed to read line");
+
+    let a: f32 = match x.trim().parse() {
+        Ok(num) => num,
+        Err(_)=> continue,
+    };
+    return a
+    }
+    
+}
+
+fn main() {
+   
+    println!("enter the input voltage");
+
+    let u_in  = input();
+
+    println!("enter the output voltage");
+
+    let u_out  = input();
+
+    if u_in / 2.0 == u_out{
+        println!("any resistors with the same resistance values");
+        return;
+    }
+    
+    if u_in/u_out - 1.0 > 10.0 || u_out/u_in - 1.0 > 0.1{
+        println!("the resistance ratio should be < 10 or > 0.1, but with these values ​​this condition is violated");
+        return;
+    }
+    if u_in / 2.0 < u_out{
+        return;
+    }
+    else{
+
+    let mut e96:Vec<f32> = vec![100.00,	102.00,	105.00,	107.00,	110.00,	113.00,
+    115.00,	118.00,	121.00,	124.00,	127.00,  130.00,
+    133.00,	137.00,	140.00,	143.00,	147.00,	150.00,
+    154.00,	158.00,	162.00,	165.00,	169.00,	174.00,
+    178.00,	182.00,	187.00,	191.00,	196.00,	200.00,
+    205.00,	210.00,	215.00,	221.00,	226.00,	232.00,
+    237.00,	243.00,	249.00,	255.00,	261.00,	267.00,
+    274.00,	280.00,	287.00,	294.00, 301.00,	309.00,
+    316.00,	324.00,	332.00,	340.00,	348.00,	357.00,
+    365.00,	374.00,	383.00,	392.00,	402.00,	412.00,
+    422.00,	432.00,	442.00,	453.00,	464.00,	475.00,
+    487.00, 499.00,	511.00,	523.00,	536.00,	549.00,
+    562.00, 576.00,	590.00,	604.00,	619.00, 634.00,
+    649.00,	665.00, 681.00,	698.00,	715.00,	732.00,
+    750.00,	768.00, 787.00,	806.00,	825.00,	845.00,
+    866.00,	887.00,	909.00,	931.00,	953.00,	976.00];
+    let mut values:Vec<(f32,f32)> = vec![];
+    for i in 0..e96.len(){
+        e96.push(e96[i]*10.0);
+    }
+    match e96.get(191){
+       Some(num) => println!("The third element is {num}"),
+       None => println!("There is no element."),
+    }
+
+        for i in 0..e96.len()/2{
+            let r2 = e96[i];
+            let r1 = (u_in/u_out - 1.0)*r2;
+            if binary_search(&e96, r1){
+                let value = (r1,r2);
+                values.push(value);
+            }
+            else {
+                continue;
+            }
+        }
+        for i in 0..values.len(){
+            println!("Exact match was found! \n R1 : {} \t {}", values[i].0, values[i].1);
+        }
+    }
+    println!("input voltage - {u_in} V");
+
+    println!("output voltage - {u_out} V");
+
+    return;
+}
+
+
+fn binary_search(arr: &Vec<f32>, find:f32) -> bool{
+    let mut l:usize = 0;
+    let mut r  = arr.len()-1;
+    let mut mid = r/2;
+    while l+1 != r{
+        if arr[mid] < find{
+            l = mid;
+            mid = (l+r)/2;
+        }
+        else if arr[mid] > find{
+            r = mid;
+            mid = (l+r)/2;
+        }
+        else if arr[mid] == find{
+            return true;
+        }
+    }
+    if l+1 == r {
+        if arr[l] == find{
+            return true
+        }
+        else if arr[r] == find{
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    else{
+        return false;
+    }
+   
+}
+
